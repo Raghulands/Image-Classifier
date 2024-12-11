d46b33ac8f1bbf0ee81ea546126a08726f8c1c35
Ragu <111031929+Raghulands@users.noreply.github.com>
2024-12-12 00:28:54 +0530

    Add files via upload

---
+++ predict.py
@@ -0,0 +1,17 @@
+import tensorflow as tf 
+classifierLoad = tf.keras.models.load_model('model.h5')
+
+import numpy as np
+from keras.preprocessing import image
+test_image = image.load_img('Predict.jpg', target_size = (200,200))
+#test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis=0)
+result = classifierLoad.predict(test_image)
+if result[0][1] == 1:
+    print("ROSE")
+elif result[0][0] == 1:
+    print("DAISY")
+elif result[0][2] == 1:
+    print("SUNFLOWER")
+elif result[0][3] == 1:
+    print("DANDELION")
